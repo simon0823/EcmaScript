70496f1b8d820516e19c6b2221f05d029cf8fa02
simon0823 <simon0823@naver.com>
2018-09-23 21:24:10 +0900

    2018-09-23

---
+++ es6/templateLiterals.js
@@ -0,0 +1,10 @@
+var first = 'Seonghyun';
+var last = 'Ryu';
+var id = 'simon0823';
+var domain = 'naver.com';
+
+var name = `Your name is ${first} ${last}`;
+var email = `${id}@${domain}`;
+
+console.log(name);
+console.log(email);
